65915dc89e6d96af258b4ac5e3dfc419610622ac
haktanbasak <haktanbasak9@gmail.com>
2021-07-12 19:27:26 +0300

    12.07.2021

---
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            Context c = new Context();
+            var adminUserInfo = c.Admins.FirstOrDefault(x=>x.AdminUsername == p.AdminUsername && x.AdminPassword == p.AdminPassword);
+            if (adminUserInfo != null)
+            {
+                FormsAuthentication.SetAuthCookie(adminUserInfo.AdminUsername,false);
+                Session["AdminUsername"] = adminUserInfo.AdminUsername;
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
